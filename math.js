0d58847bb08ff9755cf5f71e77d5e8bc9cda03b8
Erick Merchant <erickmerchant@gmail.com>
2017-09-19 21:21:10 -0700

    Make concise

--- math.js
+++ math.js
@@ -1,76 +1,68 @@
-const IS_PARENED = /\([^()]*\)/
+const input = process.argv.slice(2).join(' ')
+
 const PARENED = /\(([^()]*)\)/
-const TOKENS = /([0-9.]+|\*|\/|\+|-)/ // @TODO include all numbers
-let input = process.argv.slice(2).join(' ')
 
-console.log(evaluate(input))
+const TOKENS = /([0-9.]+|\*|\/|\+|-)/g
 
-function evaluate (expression) {
-  while (IS_PARENED.test(expression)) {
-    expression = expression.replace(PARENED, function (result, match) {
-      return evaluate(match)
+const ERROR_MESSAGE = 'Invalid Expression'
+
+const wrapOperation = (operator) => {
+  return (a, b) => {
+    a = Number(a)
+    b = Number(b)
+
+    if (Number.isNaN(a) || Number.isNaN(b)) {
+      throw new Error(ERROR_MESSAGE)
+    }
+
+    return operator(a, b)
+  }
+}
+
+const operations = {
+  '*': wrapOperation((a, b) => a * b),
+  '/': wrapOperation((a, b) => a / b),
+  '+': wrapOperation((a, b) => a + b),
+  '-': wrapOperation((a, b) => a - b)
+}
+
+const evaluate = (expression) => {
+  while (PARENED.test(expression)) {
+    expression = expression.replace(PARENED, (result, match) => {
+      return ' ' + evaluate(match) + ' '
     })
   }
 
-  if (expression.trim() === '') {
-    return ' '
-  }
-
-  let tokens = expression.replace(TOKENS, function (result, match) {
+  let tokens = expression.replace(TOKENS, (result, match) => {
     return ' ' + match + ' '
   })
-    .split(/\s+/)
-    .filter(token => token.trim() !== '')
+  .split(/\s+/)
+  .filter(token => token.trim() !== '')
+
   let result
 
-  do {
+  while (tokens.length) {
     let token = tokens.shift()
+    let temp
 
-    if (result != null) {
-      switch (true) {
-        case token === '*' && isNumber(tokens[0]):
-          result *= Number(tokens.shift())
-          break
-
-        case token === '/' && isNumber(tokens[0]):
-          result /= Number(tokens.shift())
-          break
-
-        case token === '+' && isNumber(tokens[0]):
-          result += Number(tokens.shift())
-          break
-
-        case token === '-' && isNumber(tokens[0]):
-          result -= Number(tokens.shift())
-          break
-
-        default:
-          throw new Error('Malformed Expression')
-      }
-    } else {
-      switch (true) {
-        case token === '+' && isNumber(tokens[0]):
-          result = Number(tokens.shift())
-          break
-
-        case token === '-' && isNumber(tokens[0]):
-          result = 0 - Number(tokens.shift())
-          break
-
-        case isNumber(token):
-          result = Number(token)
-          break
-
-        default:
-          throw new Error('malformed expression')
+    if (result != null && ['*', '/', '+', '-'].includes(token)) {
+      temp = operations[token](result, tokens.shift())
+    } else if (result == null) {
+      if (['+', '-'].includes(token)) {
+        temp = operations[token](0, tokens.shift())
+      } else if (!Number.isNaN(Number(token))) {
+        temp = Number(token)
       }
     }
+
+    if (temp == null) {
+      throw new Error(ERROR_MESSAGE)
+    }
+
+    result = temp
   }
-  while (tokens.length)
 
-  return result
+  return result != null ? result : ''
 }
 
-function isNumber (val) {
-  return Number(val) === Number(val)
-}
+console.log(evaluate(input))
